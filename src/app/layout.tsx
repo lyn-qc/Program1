2f4e83e4ceb7ce90077c0744743a030633222ed0
lyn <2660023684@qq.com>
2025-04-02 18:06:42 +0800

    lyn chg2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.scss";
-import { AntdRegistry } from "@ant-design/nextjs-registry";
+import './globals.css'
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,9 +28,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
 
-        <AntdRegistry>
-          {children}
-        </AntdRegistry>
+
+        {children}
+
 
       </body>
     </html>
